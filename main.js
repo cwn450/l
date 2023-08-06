2dcf18f14a14ea84712d4f371e2f09a2711bec39
cwn450 <118194942+cwn450@users.noreply.github.com>
2023-08-06 20:38:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+nosex=0
+nosey=0
+
+function preload(){
+clown_nose=loadImage('https://i.postimg.cc/VvxDpKDD/pngtree-clown-red-nose-animation-illustration-image-1243961.jpg')
+}
+
+function setup(){
+canvas=createCanvas(300,300)
+canvas.center()
+video=createCapture(VIDEO)
+video.size(300,300)
+video.hide()
+
+poseNet=ml5.poseNet(video,modelLoaded)
+poseNet.on('pose' , gotPoses)
+}
+
+function modelLoaded(){
+    console.log("modelLoaded")
+}
+function draw(){
+image(video,0,0,300,300)
+/*fill(255,0,0);
+stroke(255,0,0);
+circle(nosex,nosey,20);*/
+image(clown_nose,nosex-15,nosey-15,30,30)
+}
+
+function take_snapshot(){
+    save('student_name.png')
+}
+
+function gotPoses(results)
+{
+    if(results.length>0)
+    {
+        console.log(results);
+       nosex=results[0].pose.nose.x;
+       nosey=results[0].pose.nose.y;
+
+       console.log("nosex="+nosex);
+       console.log("nosey="+nosey);
+    }
+}
